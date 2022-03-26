6c3cfc32c1022ab4be0b7656fa39fdab84681dab
Pat Raynor <raynorpat@raynorpat.com>
2022-03-26 13:26:55 -0400

    Remove legacy pxelinux configs, rename WDS and MDT folder to just MDT

--- WDS and MDT/setup_wds_pxelinux.bat
+++
@@ -1,6 +0,0 @@
-wdsutil /set-server /bootprogram:boot\x86\pxelinux.com /architecture:x86
-wdsutil /set-server /bootprogram:boot\x64\pxelinux.com /architecture:x64
-wdsutil /set-server /N12bootprogram:boot\x86\pxelinux.com /architecture:x86
-wdsutil /set-server /N12bootprogram:boot\x64\pxelinux.com /architecture:x64
-wdsutil /set-server /bootprogram:boot\x64\syslinux.efi  /architecture:x64UEFI
-wdsutil /set-server /N12bootprogram:boot\x64\syslinux.efi  /architecture:x64UEFI
