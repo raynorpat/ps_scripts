b09c16ad43f558be47916f77005296ad2ee6df9c
Pat Raynor <raynorpat@gmail.com>
2023-10-27 14:11:19 -0400

    Wrote Evergreen install script for Microsoft PowerToys

---
+++ WindowsScripts/MDT/Applications/Microsoft PowerToys/Install.ps1
@@ -0,0 +1,54 @@
+﻿Clear-Host
+Write-Verbose "Setting Arguments" -Verbose
+$StartDTM = (Get-Date)
+
+Write-Verbose "Installing Modules" -Verbose
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+# Trust PowerShell Gallery
+If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+}
+
+# Install or update Evergreen module
+$Installed = Get-Module -Name "Evergreen" -ListAvailable | `
+    Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
+    Select-Object -First 1
+$Published = Find-Module -Name "Evergreen"
+if ($Null -eq $Installed) {
+    Install-Module -Name "Evergreen"
+} elseIf ([System.Version]$Published.Version -gt [System.Version]$Installed.Version) {
+    Update-Module -Name "Evergreen"
+}
+
+# Install PowerToys
+$Vendor = "Microsoft"
+$Product = "MicrosoftPowerToys"
+$PackageName = "MicrosoftPowerToys_x64"
+$Evergreen = Get-EvergreenApp -Name $Product | Where-Object {$_.Architecture -eq "x64" -and $_.URI -like "*.exe" -and $_.InstallerType -like "Default" }
+$Version = $Evergreen.Version
+$URL = $Evergreen.uri
+$InstallerType = "exe"
+$Source = "$PackageName" + "." + "$InstallerType"
+$LogPS = "${env:SystemRoot}" + "\Temp\$Vendor $Product $Version PS Wrapper.log"
+$ProgressPreference = 'SilentlyContinue'
+$UnattendedArgs = '/install /quiet /norestart'
+
+Start-Transcript $LogPS | Out-Null
+ 
+If (!(Test-Path -Path $Version)) {New-Item -ItemType directory -Path $Version | Out-Null}
+ 
+CD $Version
+ 
+Write-Verbose "Downloading $Vendor $Product $Version" -Verbose
+If (!(Test-Path -Path $Source)) {Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $Source}
+        
+Write-Verbose "Starting Installation of $Vendor $Product $Version" -Verbose
+(Start-Process "$PackageName.$InstallerType" $UnattendedArgs -Wait -Passthru).ExitCode
+
+Write-Verbose "Stop logging" -Verbose
+$EndDTM = (Get-Date)
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalSeconds) Seconds" -Verbose
+Write-Verbose "Elapsed Time: $(($EndDTM-$StartDTM).TotalMinutes) Minutes" -Verbose
+Stop-Transcript
